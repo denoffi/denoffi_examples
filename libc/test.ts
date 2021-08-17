df7b74b5acceaf4097df3e31d15330e30053a7a1
迷渡 <justjavac@gmail.com>
2021-08-17 10:37:52 +0800

    use libc.so.6 on linux
    Signed-off-by: 迷渡 <justjavac@gmail.com>

--- libc/test.ts
+++ libc/test.ts
@@ -3,7 +3,7 @@ import { assertEquals } from "https://deno.land/std@0.103.0/testing/asserts.ts";
 Deno.test({
   name: "libc",
   fn() {
-    const libNmae = Deno.build.os === "linux" ? "libc.so" : "libc.dylib";
+    const libNmae = Deno.build.os === "linux" ? "libc.so.6" : "libc.dylib";
     const lib = Deno.dlopen(libNmae, {
       abs: { parameters: ["i32"], result: "i32" },
     });
