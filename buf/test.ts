f2509c80b719376b7f777915891a75ac94b5b173
迷渡 <justjavac@gmail.com>
2021-10-09 10:09:26 +0800

    add buffer
    Signed-off-by: 迷渡 <justjavac@gmail.com>

---
+++ buf/test.ts
@@ -0,0 +1,15 @@
+import { assertEquals } from "https://deno.land/std@0.103.0/testing/asserts.ts";
+import { getLibPath } from "../utils.ts";
+
+Deno.test("buf", () => {
+  const lib = Deno.dlopen(getLibPath("buf"), {
+    print_buffer: { parameters: ["buffer", "usize"], result: "void" },
+  });
+
+  const buffer = new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8]);
+  const result = lib.symbols.print_buffer(buffer, buffer.length);
+
+  assertEquals(result, null);
+
+  lib.close();
+});
