4886dbe200abf1c440fcac7ca671485f29e181a3
justjavac <justjavac@gmail.com>
2022-03-16 10:25:21 +0800

    use pointer

--- buf/test.ts
+++ buf/test.ts
@@ -3,7 +3,7 @@ import { getLibPath } from "../utils.ts";
 
 Deno.test("buf", () => {
   const lib = Deno.dlopen(getLibPath("buf"), {
-    print_buffer: { parameters: ["buffer", "usize"], result: "void" },
+    print_buffer: { parameters: ["pointer", "usize"], result: "void" },
   });
 
   const buffer = new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8]);
